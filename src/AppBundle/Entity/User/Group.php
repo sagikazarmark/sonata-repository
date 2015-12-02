985768517ca2a9248a7166d466b9c20ec24ca1de
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2015-12-02 18:02:18 +0100

    Add basic Sonata User features
    Change order of annotations
    
    Add User related migrations

---
+++ src/AppBundle/Entity/User/Group.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Entity\User;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as JMS;
+use Sonata\UserBundle\Entity\BaseGroup;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table("fos_user_group")
+ *
+ * @JMS\ExclusionPolicy("all")
+ * @JMS\XmlRoot("group")
+ */
+class Group extends BaseGroup
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     * @ORM\Id
+     *
+     * @JMS\Expose
+     * @JMS\Groups({"sonata_api_read", "sonata_api_write", "sonata_search"})
+     * @JMS\Since("1.0")
+     * @JMS\Type("integer")
+     * @JMS\XmlAttributeMap
+     */
+    protected $id;
+}
